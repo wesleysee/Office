fa2b02f75eb868e2160ce1e6c474ae6ce588fec9
Wesley Spencer See <wesleysee@gmail.com>
2018-12-23 21:17:56 -0600

    add link to excel

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -58,8 +58,14 @@ WHERE t.date >= \'' + @start_date.to_s + '\' AND t.date <= \'' + @end_date.to_s 
 t GROUP BY t.name
 ORDER BY t.name ASC')
 
+    path = Rails.application.config.time_records_folder_location
+    xlsx_end_date = @end_date - 1.days
+
+    @excel_file = path + start_date.to_s + '_' + end_date.to_s + '.xlsx'
+    @excel_exists = File.file?(@excel_file)
+
     respond_to do |format|
-      format.html # show_monthly_report.html.erb
+      format.html # show_weekly_report.html.erb
     end
   end
 
